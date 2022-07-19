c859cd6c20b36ef582603296e90478e81c37c7ee
500ping <ndt512x@gmail.com>
2022-07-19 23:34:30 +0700

    airbnb clone navbar and hero

---
+++ airbnb_clone/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img className="nav--logo" src="/src/assets/images/airbnb-logo.png" />
+        </nav>
+    )
+}
